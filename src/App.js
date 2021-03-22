f457d775ebcc593026681e491c80ad84b056c313
Vincent <vincent.bezault@icloud.com>
2021-03-22 15:30:44 +0100

    add user page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { SignIn } from "./pages/Sign-in/Sign-in";
 import { LogIn } from "./pages/Log-in/Log-in";
+import { User } from "./pages/User/User";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Route path="/" exact component={SignIn} />
         <Route path="/LogIn" exact component={LogIn} />
+        <Route path="/User" exact component={User} />
       </Router>
     </div>
   );
---
+++ src/pages/User/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export class User extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Bienvenue</h1>
+            </div>
+        )
+    }
+}
+
